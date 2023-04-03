9bae1963ac51c14f81a20be4cc51eb0bd658540c
Ezequiel <eolavaca@gmail.com>
2023-04-03 17:10:10 -0300

    build with new url

--- dist/assets/404-5ac141e6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,a as s}from"./index-8484a920.js";const c={},n=s("h1",null,"Not Found - 404",-1),a=[n];function _(r,d,l,p,i,u){return o(),t("div",null,a)}const h=e(c,[["render",_]]);export{h as default};
