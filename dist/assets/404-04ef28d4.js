ab57ff9f3b546e10a4d74605ab4e1a3c8c88d71f
Ezequiel <eolavaca@gmail.com>
2023-04-03 18:40:22 -0300

    deploying fav issue

---
+++ dist/assets/404-04ef28d4.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as s}from"./index-ccede38d.js";const c={},n=s("h1",null,"Not Found - 404",-1),a=[n];function _(r,d,l,p,i,u){return o(),t("div",null,a)}const h=e(c,[["render",_]]);export{h as default};
